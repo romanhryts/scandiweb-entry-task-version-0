8d167ece907435996ac4f939af787fe6bd2d61f3
Roman Grycyshyn <rogry@MBPRo-Gry.fritz.box>
2022-04-07 22:26:05 +0200

    Complete currency converter in header

--- js/category-items.js
+++ js/category-items.js
@@ -1,9 +1,28 @@
 (function () {
   let items;
+  let activeCurrency = "price";
+  let activeCurrencyIcon = "currency";
+
+  const rateEuro = 0.91;
+  const rateYen = 123.88;
+
 
   async function loadCategoryItems() {
     const response = await fetch("category-items.json");
-    items = await response.json();
+    const data = await response.json();
+
+    items = data.map((item) => {
+      item.euroPrice = item.price * rateEuro;
+      item.yenPrice = item.price * rateYen;
+      return item;
+    });
+
+    items = data.map((item) => {
+      item.euroCurrency = "€";
+      item.yenCurrency = "¥";
+      return item;
+    })
+
     renderCategoryItems();
   }
 
@@ -15,74 +34,39 @@
             <div class="category__item">
                <img src="${item.imgUrl}" alt="${item.title}" class="category__item-icon">
                 <h3 class="category__item-name">${item.title}</h3>
-                <h4 class="category__item-price">${item.euroPrice ? item.euroPrice.toFixed(2) : item.yenPrice ? item.yenPrice.toFixed(2) : item.price.toFixed(2)}</h4>
+                <h4 class="category__item-price">${item[activeCurrencyIcon] + item[activeCurrency].toFixed(2)}</h4>
             </div>
             `;
     }
   }
 
-  document.querySelector(".currency-converter__EUR").addEventListener("click", function () {
-    const rateEuro = 0.91;
-    for (const item of items) {
-      item.euroPrice = item.price * rateEuro;
+  function updateCurrency(newCurrency, newCurrencyIcon) {
+    if (activeCurrency !== newCurrency && activeCurrencyIcon !== newCurrencyIcon) {
+      activeCurrency = newCurrency;
+      activeCurrencyIcon = newCurrencyIcon;
+      renderCategoryItems();
     }
-    renderCategoryItems();
-  });
+  }
+
+  document.querySelector(".currency-converter__USD").addEventListener("click", function () {
+      updateCurrency("price", "currency");
+    });
+
+  document.querySelector(".currency-converter__EUR").addEventListener("click", function () {
+      updateCurrency("euroPrice", "euroCurrency");
+    });
 
   document.querySelector(".currency-converter__JPY").addEventListener("click", function () {
-    const rateYen = 123.88;
-    for (const item of items) {
-      item.yenPrice = item.price * rateYen;
-    }
-})
-
-  /*
- document.querySelector(".currency-converter__EUR").addEventListener("click", convertCurrency);
- document.querySelector(".currency-converter__USD").addEventListener("click", convertCurrency);
- document.querySelector(".currency-converter__JPY").addEventListener("click", convertCurrency);
-*/
-  /*
-  function convertCurrency() {
-    const euroRate = 0.91;
-    const yenRate = 123.83;
-    if (euro) {
-      for (const item of items) {
-        item.currentPrice = item.price * euroRate;
-      } 
-          renderCategoryItems();
-    } else if (dollar) {
-      for (const item of items) {
-        item.currentPrice = item.price;
-      }
-          renderCategoryItems();
-    } else if (yen) {
-      for (const item of items) {
-        item.currentPrice = item.price * yenRate;
-      }
-          renderCategoryItems();
-    }
-        renderCategoryItems();
-  }
-  */
-
-  /* document
-    .querySelector(".currency-converter__EUR")
-    .addEventListener("click", convertToEuro);
-  */
-  // document.querySelector(".currency-convertor__USD").addEventListener("click", convertToDollar);
-
-  /* document
-    .querySelector(".currency-convertor__JPY")
-    .addEventListener("click", convertToYen); */
-
-  /*   function convertToYen() {
-    const rateYen = 123.83;
-    for (const item of items) {
-      item.priceYen = item.price * rateYen;
-    }
-    renderCategoryItems();
-
-  } */
+      updateCurrency("yenPrice", "yenCurrency");
+    });
 
   loadCategoryItems();
+
+  const currencyMenu = document.querySelector(".icon__dollar");
+  const currencyBody = document.querySelector(".header__currency-converter");
+  const currencyMenuArrow = document.querySelector(".icon__arrow");
+  currencyMenu.addEventListener("click", function (e) {
+    currencyBody.classList.toggle("active");
+    currencyMenuArrow.classList.toggle("active");
+  })
 })();
